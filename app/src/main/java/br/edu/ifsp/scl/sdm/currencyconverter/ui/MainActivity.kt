8705195eabf0e31039e535a87a4172a6267f6208
Celestino <001celestino@gmail.com>
2025-04-29 20:12:59 -0300

    feat: implementing convert service - part 2

--- app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package br.edu.ifsp.scl.sdm.currencyconverter.ui
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.ArrayAdapter
 import br.edu.ifsp.scl.sdm.currencyconverter.R
 import br.edu.ifsp.scl.sdm.currencyconverter.databinding.ActivityMainBinding
 import br.edu.ifsp.scl.sdm.currencyconverter.model.livedata.CurrencyConverterLiveData
+import br.edu.ifsp.scl.sdm.currencyconverter.service.ConvertService
 import br.edu.ifsp.scl.sdm.currencyconverter.service.CurrenciesService
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +20,18 @@ class MainActivity : AppCompatActivity() {
 
     private val currenciesServiceIntent by lazy {
         Intent(this, CurrenciesService::class.java)
+    }
+
+    private var convertService : ConvertService? = null
+    private val convertServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            convertService = (p1 as ConvertService.ConvertServiceBinder).getConvertService()
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            TODO("Not yet implemented")
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +56,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             convertBt.setOnClickListener {
-
+                convertService?.convert(fromQuote,toQuote, amountTiet.text.toString())
             }
         }
         CurrencyConverterLiveData.currenciesLiveData.observe(this) { currencyList ->
@@ -55,10 +71,29 @@ class MainActivity : AppCompatActivity() {
                 toQuote = quote
             }
         }
+        CurrencyConverterLiveData.conversionResultLiveData.observe(this) { conversionResult ->
+            with(amb) {
+                conversionResult.rates.values.first().rateForAmount.also {
+                    resultTiet.setText(it)
+                }
+            }
+        }
+
 
         startService(currenciesServiceIntent)
     }
 
+    override fun onStart() {
+        super.onStart()
+        Intent(this@MainActivity, ConvertService::class.java).also { intent ->
+            bindService(intent, convertServiceConnection, BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(convertServiceConnection)
+    }
     override fun onDestroy() {
         super.onDestroy()
         stopService(currenciesServiceIntent)
